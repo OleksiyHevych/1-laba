3af34fcd2a159d243710a78f155981e704d4a0b3
Oleksiy <oleksiyhevych@gmail.com>
2023-06-16 21:07:11 +0300

    Initial commit

--- task7.js
+++ task7.js
@@ -1,72 +1,72 @@
 const mod = require("./task7/modules");
 // Додавання замовників
-let c1 = mod.add_Customer("Сергій");
-let c2 = mod.add_Customer("Данило");
+let c1 = mod.add_Customer("Назар");
+let c2 = mod.add_Customer("Артур");
 let c3 = mod.add_Customer();
 
 // Список замовників
 mod.get_Customer_List();
 
 // Видалення замовників
-console.log("Видалення замовника: Сергій");
-mod.remove_Customer("Сергій");
+console.log("Видалення замовника: Назар");
+mod.remove_Customer("Назар");
 
 // Список замовників
 mod.get_Customer_List();
 
 // Зміна замовників
-console.log("Зміна замовника: Данило");
-mod.edit_Customer("Данило","Новий замовник");
+console.log("Зміна замовника: Артур");
+mod.edit_Customer("Артур","Новий замовник");
 
 // Список замовників
 mod.get_Customer_List();
 
 // Пошук замовників
-let custom1 = mod.find_Customer("Абоба");
-console.log(`Пошук замовника Абоба: ${custom1 !== -1 ? "знайдено" : "не знайдено"}`);
+let custom1 = mod.find_Customer("Антон");
+console.log(`Пошук замовника Антон: ${custom1 !== -1 ? "знайдено" : "не знайдено"}`);
 let custom2 = mod.find_Customer("Новий замовник");
 console.log(`Пошук замовника Новий замовник: ${custom2 !== -1 ? "знайдено" : "не знайдено"}`);
 
 ////////////////////////////////////////////////////////////////////////////////////
 // Додавання виконавців
-mod.add_Executor("ВАлодя");
-mod.add_Executor("Сєрий");
-mod.add_Executor("Петросянич");
+mod.add_Executor("Євстахій");
+mod.add_Executor("Карлсон");
+mod.add_Executor("Галина");
 
 // Список виконавців
 mod.get_Executor_List();
 
 // Видалення виконавців
-console.log("Видалення виконавців: Сєрий");
-mod.remove_Executor("Сєрий");
+console.log("Видалення виконавців: Карлсон");
+mod.remove_Executor("Карлсон");
 
 // Список виконавців
 mod.get_Executor_List();
 
 // Редагування виконавців
-console.log("Редагування виконавців: ВАлодя");
-mod.edit_Executor("ВАлодя","Марта петрівна");
+console.log("Редагування виконавців: Євстахій");
+mod.edit_Executor("Євстахій","Світлана Миколаївна");
 
 // Список виконавців
 mod.get_Executor_List();
 
 // Пошук виконавців
-let exec1 = mod.find_Executor("Марта петрівна");
-console.log(`Пошук виконавця - Марта петрівна: ${exec1 !== -1 ? "знайдено" : "не знайдено"}`);
-let exec2 = mod.find_Executor("Костянтин львович");
-console.log(`Пошук виконавця - Костянтин львович: ${exec2 !== -1 ? "знайдено" : "не знайдено"}`);
+let exec1 = mod.find_Executor("Світлана Миколаївна");
+console.log(`Пошук виконавця - Світлана Миколаївна: ${exec1 !== -1 ? "знайдено" : "не знайдено"}`);
+let exec2 = mod.find_Executor("Костянтин Львович");
+console.log(`Пошук виконавця - Костянтин Львович: ${exec2 !== -1 ? "знайдено" : "не знайдено"}`);
 
 ////////////////////////////////////////////////////////////////////////////////////
 //створення замовників
-let customer1 = mod.add_Customer("Петро");
-let customer2 = mod.add_Customer("Іван");
+let customer1 = mod.add_Customer("Олег");
+let customer2 = mod.add_Customer("Олександр");
 //створення замовників
-let executor2 = mod.add_Customer("Поле");
-let executor3 = mod.add_Customer("Нива");
+let executor1 = mod.add_Customer("Поле");
+let executor2 = mod.add_Customer("Нива");
 // Додавання проєктів
-mod.add_Project("Garena", customer1, executor2);
-mod.add_Project("FreFire", customer2, executor2);
-mod.add_Project("BottleRoyal", customer2, executor3);
+mod.add_Project("PC", customer1, executor1);
+mod.add_Project("VHS", customer2, executor1);
+mod.add_Project("DVD", customer2, executor2);
 
 // Список проєктів
 mod.get_Projects_List();
@@ -76,15 +76,15 @@ mod.get_Projects_List_CUSTOMER(customer2)
 mod.get_Projects_List_EXECUTOR(executor2)
 
 // Видалення проєктів
-console.log("Видалення проектів: FreFire");
-mod.remove_Project("FreFire", customer2, executor2);
+console.log("Видалення проектів: VHS");
+mod.remove_Project("VHS", customer2, executor1);
 
 // Список проєктів
 mod.get_Projects_List();
 
 // Редагування проєктів
-console.log("Редагування проєктів: BottleRoyal");
-mod.edit_Project("BottleRoyal", customer2, executor3,"FreFire", customer2, executor2);
+console.log("Редагування проєктів: PC");
+mod.edit_Project("PC", customer1, executor1,"VHS", customer2, executor1);
 
 // Список проєктів
 mod.get_Projects_List();
--- task7/modules.js
+++ task7/modules.js
@@ -1,12 +1,6 @@
-const customer   = require("./3customer");
 const executor   = require("./1executant");
 const project    = require("./2project");
-
-exports.find_Customer      = customer.find_Customer;
-exports.add_Customer       = customer.add_Customer;
-exports.remove_Customer    = customer.remove_Customer;
-exports.edit_Customer      = customer.edit_Customer;
-exports.get_Customer_List  = customer.get_Customer_List;
+const customer   = require("./3customer");
 
 exports.find_Executor      = executor.find_Executor;
 exports.add_Executor       = executor.add_Executor;
@@ -20,3 +14,9 @@ exports.edit_Project                = project.edit_Project;
 exports.get_Projects_List           = project.get_Projects_List;
 exports.get_Projects_List_CUSTOMER  = project.get_Projects_List_CUSTOMER;
 exports.get_Projects_List_EXECUTOR  = project.get_Projects_List_EXECUTOR;
+
+exports.find_Customer      = customer.find_Customer;
+exports.add_Customer       = customer.add_Customer;
+exports.remove_Customer    = customer.remove_Customer;
+exports.edit_Customer      = customer.edit_Customer;
+exports.get_Customer_List  = customer.get_Customer_List;
